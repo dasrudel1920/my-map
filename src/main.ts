dc9feedc238d283fd519aef718f4b75c86e4499c
dasrudel1920 <156120259+dasrudel1920@users.noreply.github.com>
2024-01-12 00:23:33 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,64 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import {ActionMessage} from "@workadventure/iframe-api-typings";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
+/**
+ * Utility function to display the correct door image depending on the state of the door.
+ */
+function displayDoor(state: boolean) { 
+    if (state === true) {
+        WA.room.showLayer('door/door_opened');
+        WA.room.hideLayer('door/door_closed');
+    } else {
+        WA.room.hideLayer('door/door_opened');
+        WA.room.showLayer('door/door_closed');
+    }
+}
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
+WA.onInit().then(async () => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
-
-    WA.room.area.onLeave('clock').subscribe(closePopup)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    // Most notably for us, it is used to generate the "Configure the room" menu and to handle the "bell".
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
-}).catch(e => console.error(e));
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
+    // The doorState variable contains the state of the door.
+    // True: the door is open
+    // False: the door is closed
+    // Upon load, the function bellow is called to initiate the door state.
+    displayDoor(WA.state.doorState);
+
+    // After load, we listen to variable change to display the correct door image.
+    WA.state.onVariableChange('doorState').subscribe((doorState) => {
+        // Each time the "doorState" variable changes, we call the "displayDoor" function to update the door image visually.
+        displayDoor(doorState as boolean);
+    });
+
+    let openCloseMessage: ActionMessage | undefined;
+
+    // When someone walks on the doorstep (inside the room), we display a message to explain how to open or close the door
+    WA.room.onEnterLayer('doorsteps/inside_doorstep').subscribe(() => {
+        openCloseMessage = WA.ui.displayActionMessage({
+            message: "Press 'space' to open/close the door",
+            callback: () => {
+                WA.state.doorState = !WA.state.doorState;
+            }
+        });
+    });
+
+    // When someone leaves the doorstep (inside the room), we remove the message
+    WA.room.onLeaveLayer('doorsteps/inside_doorstep').subscribe(() => {
+        if (openCloseMessage !== undefined) {
+            openCloseMessage.remove();
+        }
+    });
+}).catch(e => console.error(e));
 
 export {};
