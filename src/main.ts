5c373f130a2fbc2f2c065c9e74d17e02027bdbd1
dasrudel1920 <156120259+dasrudel1920@users.noreply.github.com>
2024-01-12 00:41:55 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,41 +5,29 @@ import {ActionMessage} from "@workadventure/iframe-api-typings";
 
 console.log('Script started successfully');
 
-/**
- * Utility function to display the correct door image depending on the state of the door.
- */
-function displayDoor(state: boolean) { 
-    if (state === true) {
-        WA.room.showLayer('door/door_opened');
-        WA.room.hideLayer('door/door_closed');
-    } else {
-        WA.room.hideLayer('door/door_opened');
-        WA.room.showLayer('door/door_closed');
-    }
-}
-
 // Waiting for the API to be ready
 WA.onInit().then(async () => {
     console.log('Scripting API ready');
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    // Most notably for us, it is used to generate the "Configure the room" menu and to handle the "bell".
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
+    await WA.players.configureTracking({
+        players: true,
+        movement: false,
+    });
 
     // The doorState variable contains the state of the door.
     // True: the door is open
     // False: the door is closed
-    // Upon load, the function bellow is called to initiate the door state.
-    displayDoor(WA.state.doorState);
-
-    // After load, we listen to variable change to display the correct door image.
+    // We listen to variable change to display the correct door image.
     WA.state.onVariableChange('doorState').subscribe((doorState) => {
-        // Each time the "doorState" variable changes, we call the "displayDoor" function to update the door image visually.
-        displayDoor(doorState as boolean);
+        displayDoor(doorState);
     });
+
+    displayDoor(WA.state.doorState);
 
     let openCloseMessage: ActionMessage | undefined;
 
@@ -59,6 +47,51 @@ WA.onInit().then(async () => {
             openCloseMessage.remove();
         }
     });
+
+    WA.room.onEnterLayer('meetingRoom').subscribe(() => {
+        WA.player.state.saveVariable("currentRoom", "meetingRoom", {
+            public: true,
+            persist: false
+        });
+    });
+
+    WA.room.onLeaveLayer('meetingRoom').subscribe(() => {
+        WA.player.state.saveVariable("currentRoom", undefined, {
+            public: true,
+            persist: false
+        });
+    });
+
+    // When someone walks on the doorstep (outside the room), we check if the door is closed
+    // If the door is closed, and if no one is inside (because no player has the "currentRoom" variable set to "meetingRoom"),
+    // we open the door automatically.
+    WA.room.onEnterLayer('doorsteps/outside_doorstep').subscribe(() => {
+        if (WA.state.doorState === false) {
+            const players = WA.players.list();
+            for (const player of players) {
+                if (player.state.currentRoom === "meetingRoom") {
+                    // Someone is in the room
+                    return;
+                }
+            }
+            // If no one is in the room and if the door is closed, we open it automatically
+            WA.state.doorState = true;
+        }
+    });
+
 }).catch(e => console.error(e));
+
+/**
+ * Display the correct door image depending on the state of the door.
+ */
+function displayDoor(state: unknown) {
+    if (state === true) {
+        WA.room.showLayer('door/door_opened');
+        WA.room.hideLayer('door/door_closed');
+    } else {
+        WA.room.hideLayer('door/door_opened');
+        WA.room.showLayer('door/door_closed');
+    }
+}
 
 export {};
